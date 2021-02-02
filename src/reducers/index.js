c9ccf20da875bc6eb0da8a7365b76d02052e3864
romildoneto <r7746@hotmail.com>
2021-02-02 05:21:36 -0300

    meu commit para o github

---
+++ src/reducers/index.js
@@ -0,0 +1,35 @@
+
+import {combineReducers} from "redux";
+
+
+const initialState = {
+    frutas:[
+        {nome:"banana"},
+        {nome:"maça"}
+    ]
+}
+
+function frutaReducer(state = initialState, action) {
+if (action.type === "ADICIONAR"){
+    return {frutas:[...state.frutas, {...action.value}] };
+}
+else {
+    return state;
+  }
+}
+
+function tituloReducer(state = {titulo:"Frutas"}, action) {
+if(action.type ==="ALTERAR") {
+return {titulo: action.value};
+}
+else{
+   return state;
+ }
+}
+
+
+
+
+const reducers = combineReducers({frutaReducer, tituloReducer});
+
+export default reducers;
